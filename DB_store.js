b4599b3f3a7ee6b698a4d5304d6134b6351844e9
Rodolfo Zepeda <rodolfo.z1010@gmail.com>
2023-10-01 23:56:59 -0600

    Project update, login. Not working yet, main js basis added, as well ad DB & functions js files. Rewatch recording to understand better further modifications

---
+++ DB_store.js
@@ -0,0 +1,78 @@
+const Usuarios = [
+    {
+        user: "user1",
+        password: "password1",
+        nombre: "Alice",
+        apodo: "alice123",
+        data: {
+            nombre: "Alice",
+            apellido: "Johnson",
+            email: "alice.j@example.com",
+            fecnac: "05/23/1990",
+            imagen: "https://picsum.photos/200/300",
+            sexo: "F",
+            direccion: "456 Elm Street"
+        }
+    },
+    {
+        user: "user2",
+        password: "password2",
+        nombre: "Bob",
+        apodo: "bob34",
+        data: {
+            nombre: "Bob",
+            apellido: "Smith",
+            email: "bob.smith@example.com",
+            fecnac: "08/15/1985",
+            imagen: "https://picsum.photos/200/300",
+            sexo: "M",
+            direccion: "789 Oak Avenue"
+        }
+    },
+    {
+        user: "user3",
+        password: "password3",
+        nombre: "Charlie",
+        apodo: "charlie_88",
+        data: {
+            nombre: "Charlie",
+            apellido: "Brown",
+            email: "charlie.b@example.com",
+            fecnac: "12/10/1993",
+            imagen: "https://picsum.photos/200/300",
+            sexo: "M",
+            direccion: "101 Pine Road"
+        }
+    },
+    {
+        user: "user4",
+        password: "password4",
+        nombre: "Ella",
+        apodo: "ella_rose",
+        data: {
+            nombre: "Ella",
+            apellido: "Davis",
+            email: "ella.d@example.com",
+            fecnac: "03/17/1997",
+            imagen: "https://picsum.photos/200/300",
+            sexo: "F",
+            direccion: "222 Maple Lane"
+        }
+    },
+    {
+        user: "user5",
+        password: "password5",
+        nombre: "Frank",
+        apodo: "frankie",
+        data: {
+            nombre: "Frank",
+            apellido: "Garcia",
+            email: "frank.g@example.com",
+            fecnac: "11/28/1989",
+            imagen: "https://picsum.photos/200/300",
+            sexo: "M",
+            direccion: "333 Cedar Street"
+        }
+    }
+];
+
